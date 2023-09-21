b34991972109cf40a77aad63ab0ef6fcb0d2b09e
Daniel Saldarriaga <dsaldarriaga@prevalentware.com>
2023-09-21 06:26:30 -0500

    agregar segundo card de playlist

--- components/PlaylistContainer.tsx
+++ components/PlaylistContainer.tsx
@@ -11,10 +11,16 @@ const PlaylistContainer = ({ title }: PlaylistContainer) => {
         <span className='text-white font-bold text-xl'>{title}</span>
         <span className='text-sm text-spotify-gray3'>Show all</span>
       </div>
-      <div className='flex'>
+      <div className='flex gap-4'>
         <PlaylistCard
           title={`Today's Top Hits`}
           description='Jung Kook is on top of the Hottest 50!'
+          image='/images/tth.jpg'
+        />
+        <PlaylistCard
+          title={`Rap Caviar`}
+          description='New music from Drake, Rod Wave and Doja Cat!'
+          image='/images/rc.jpg'
         />
       </div>
     </section>
