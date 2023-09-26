8737dd61681202debd0f238dc55a13b15cb40705
Daniel Saldarriaga <dsaldarriaga@prevalentware.com>
2023-09-26 07:00:01 -0500

    corregir errores de lint

--- components/ComponenteEjemplo.tsx
+++ components/ComponenteEjemplo.tsx
@@ -1,8 +1,5 @@
-function NuevoComponente(){
-    const hola = "test"
-    console.log("esto es un console log")
-    return (<div>hola mundo</div>)
-}
+const NuevoComponente = () => {
+  return <div>hola mundo</div>;
+};
 
-
-export default NuevoComponente;
+export { NuevoComponente };
