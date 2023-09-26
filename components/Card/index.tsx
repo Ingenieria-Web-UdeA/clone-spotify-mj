72953f2d9e729eafa526f9c0b277aa0fc6b5d27f
Daniel Saldarriaga <dsaldarriaga@prevalentware.com>
2023-09-26 07:26:04 -0500

    corregir titulo del card para que siempre sea Texto

--- components/Card/index.tsx
+++ components/Card/index.tsx
@@ -1,15 +1,15 @@
 import { Button } from '@/components/Button';
 
 interface CardProps {
-  title: string;
+  title?: string;
   description: string;
   buttonText: string;
 }
 
-const Card = ({ title, description, buttonText }: CardProps) => {
+const Card = ({ description, buttonText }: CardProps) => {
   return (
     <div className='flex flex-col bg-dark-gray items-start p-4 rounded-lg gap-3'>
-      <span className='font-semibold text-lg'>{title}</span>
+      <span className='font-semibold text-lg'>Texto</span>
       <span className='text-md'>{description}</span>
       <Button type='secondary' text={buttonText} />
     </div>
