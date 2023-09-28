c900e4b4261f71d042eee53eb535c0789143303b
Daniel Saldarriaga <dsaldarriaga@prevalentware.com>
2023-09-28 06:16:33 -0500

    fix build

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
   images: {
     domains: ['ingenieria-web-spotify-media.s3.amazonaws.com'],
+    unoptimized: true,
   },
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,17 @@
 import { Navbar } from '@/components/Navbar';
 import { PlaylistContainer } from '@/components/PlaylistContainer';
 import { Sidebar } from '@/components/Sidebar';
-import useSWR from 'swr';
+import { Playlists } from '@/datos';
+// import useSWR from 'swr';
 
-const fetcher = (url: string) => fetch(url).then((res) => res.json());
+// const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 const Home = () => {
-  const { data, isLoading } = useSWR('/api/data', fetcher);
+  // const { data, isLoading } = useSWR('/api/data', fetcher);
 
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
+  // if (isLoading) {
+  //   return <div>Loading...</div>;
+  // }
 
   return (
     <div className='bg-black h-screen flex text-white'>
@@ -18,8 +19,11 @@ const Home = () => {
       <main className='w-full p-2 h-full flex flex-col'>
         <Navbar />
         <div className='px-5 py-1 bg-gradient-to-br from-spotify-gray2 to-spotify-black4 h-full flex flex-col rounded-md gap-6'>
-          <PlaylistContainer data={data.playlists} title='Spotify Playlists' />
-          <PlaylistContainer data={data.focus} title='Focus' />
+          <PlaylistContainer
+            data={Playlists.playlists}
+            title='Spotify Playlists'
+          />
+          <PlaylistContainer data={Playlists.focus} title='Focus' />
         </div>
         <footer></footer>
       </main>
